d9d759108625ccadcb997c1d06678b051b77f642
solarcitizen <angel.stark@gmail.com>
2013-12-14 20:09:22 +0200

    Initial commit
    Just initial thoughts
    
    Change-Id: Icec967dc2cb1d10478af6775fed43befa96f8111
    Signed-off-by: solarcitizen <angel.stark@gmail.com>

---
+++ JavaSnippets/src/db/operations/DataBaseOperations.java
@@ -0,0 +1,10 @@
+package db.operations;
+
+public class DataBaseOperations {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ JavaSnippets/src/strings/operations/StringOperations.java
@@ -0,0 +1,10 @@
+package strings.operations;
+
+public class StringOperations {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
