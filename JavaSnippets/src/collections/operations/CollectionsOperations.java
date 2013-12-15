089cc8599bff2099813323e293a4f4a86a6c615f
solarcitizen <angel.stark@gmail.com>
2013-12-15 21:16:51 +0200

    Collections and Files initial commit
    Addition of two categories of snippets
    
    Change-Id: I28e7b8e61ad74b23f78fc7e0693194a9f525da66
    Signed-off-by: solarcitizen <angel.stark@gmail.com>

---
+++ JavaSnippets/src/collections/operations/CollectionsOperations.java
@@ -0,0 +1,10 @@
+package collections.operations;
+
+public class CollectionsOperations {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ JavaSnippets/src/files/operations/FilesOperations.java
@@ -0,0 +1,10 @@
+package files.operations;
+
+public class FilesOperations {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
